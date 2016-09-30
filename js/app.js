053f8b5340958526e731637122260f6144419b2f
Marshall Thompson <marshallthomp@gmail.com>
2016-09-30 15:04:03 -0400

    random word is generating

--- js/app.js
+++ js/app.js
@@ -2,4 +2,27 @@
   "use strict";
 
   // alert(commonWords);
+
+  var randomWord = function(){
+    // console.log('random word is working!');
+    var min = 0;
+    var max = commonWords.length;
+    var random = Math.floor(Math.random() * (max-min) + min);
+
+    var currentWord = commonWords[random];
+
+    console.log(random, currentWord);
+
+    return currentWord;
+  };
+
+  function sethtml(){}
+
+  function gameStart(){
+    randomWord();
+  }
+
+  gameStart();
+  // window.addEventListener('load', gameStart);
+
 }());
