519d7118e3bd1f75f1dddb3282836d7bf4235b10
Marshall Thompson <marshallthomp@gmail.com>
2016-09-30 15:35:14 -0400

    just saving progress

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@
 
   // alert(commonWords);
 
+  // create variables of the nodes we want to watch or change
+  var gameForm = document.getElementById('guess-form');
+
+  // go get a random word and call the setHtml function
   var randomWord = function(){
     // console.log('random word is working!');
     var min = 0;
@@ -13,16 +17,34 @@
 
     console.log(random, currentWord);
 
+    setHtml(currentWord);
+
     return currentWord;
   };
 
-  function sethtml(){}
+  // set html content of word lenth hint
+  function setHtml(word){
 
+    var wordHolder = document.getElementById('word-holder');
+
+    word.split('').forEach(function(letter){
+      wordHolder.innerHTML += '<span> _ </span>';
+      // console.log(letter);
+    });
+  }
+
+  // check player input
+  function guessCheck(){
+
+  }
+
+  // call functions to start the game with a new word
   function gameStart(){
     randomWord();
   }
 
   gameStart();
-  // window.addEventListener('load', gameStart);
+
+  gameForm.addEventListener('submit', guessCheck);
 
 }());
